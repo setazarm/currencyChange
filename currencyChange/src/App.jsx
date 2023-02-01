51007afd445aae46df0be8cdedf3b9d37c725a9f
riccaralv <riccaralv@gmail.com>
2023-02-01 23:30:28 +0100

    header help layout notFound content added

--- currencyChange/src/App.jsx
+++ currencyChange/src/App.jsx
@@ -1,13 +1,31 @@
-import react from 'react'
+import './App.css';
 
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Header from './components/Header';
+import ConvertForm from './components/ConvertForm';
+import News from './components/News';
+import Help from './components/Help';
+import Contact from './components/Contact';
+import LoginForm from './components/LoginForm';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<ConvertForm />} />
+          <Route path='news' element={<News />} />
+          <Route path='help' element={<Help />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='loginForm' element={<LoginForm />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ currencyChange/src/components/ConvertForm.jsx
@@ -0,0 +1,11 @@
+const ConvertForm = () => {
+  return (
+    <div>
+      <img
+        src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+        alt='test-image'
+      />
+    </div>
+  );
+};
+export default ConvertForm;
